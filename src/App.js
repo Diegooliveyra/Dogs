0ea8bf0edcfc0ffa20ac0f4d3cd01c7f38c246ba
Diegooliveyra <diegooliveirabryto@gmail.com>
2021-03-13 09:49:57 -0300

    adcionado os assets e feito o header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Login from './Components/Login/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './style/App.css';
 
 function App() {
   return (
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './Header.module.css';
+import { ReactComponent as Logo } from '../../Assets/dogs.svg';
 
 const Header = () => {
   return (
-    <div className={style.header}>
-      <nav>
+    <header className={style.header}>
+      <nav className={`${style.nav} + container`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link className={style.logo} to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
-          <Link to="login">Login | Criar</Link>
+          <Link className={style.login} to="login">
+            Login | Criar
+          </Link>
         </li>
       </nav>
-    </div>
+    </header>
   );
 };
 
