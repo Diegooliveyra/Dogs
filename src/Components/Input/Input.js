3e0aea330eaff337257a5038fab8162600530ed3
Diegooliveyra <diegooliveirabryto@gmail.com>
2021-03-19 20:04:21 -0300

    feito a validação do formulario de login

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import style from './Input.module.css';
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={style.wrapper}>
       <label htmlFor={name} className={style.label}>
         {label}
       </label>
-      <input id={name} name={name} type={type} className={style.input} />
-      <p className={style.error}>Preencha um valor</p>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        className={style.input}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={style.error}>{error}</p>}
     </div>
   );
 };
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,40 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import useForm from '../../Hooks/useForm';
 import Button from '../Button/Button';
 import Input from '../Input/Input';
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const username = useForm();
+  const password = useForm();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+        }),
       })
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+          return json;
+        });
+    }
   };
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuario" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuario" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       ,<Link to="/login/create">Cadastro</Link>
