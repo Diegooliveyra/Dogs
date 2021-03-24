23124f9c8197e40829a2f0f82b8f07023579b2bb
diegooliveyra <diegooliveirabryto@gmail.com>
2021-03-24 12:21:44 -0300

    adc comentarios sobre o useForm

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+//Objeto com os tipos de validaçoes, regex e mensagem de erro
 const types = {
   email: {
     regex: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
@@ -8,11 +9,11 @@ const types = {
 };
 
 const useForm = (type) => {
-  const [value, setValue] = useState('');
-  const [error, setError] = useState(null);
+  const [value, setValue] = useState(''); // Seta um estado para o valor digitado nos inputs
+  const [error, setError] = useState(null); // seta um erro conforme valor digitado nos inputs
 
   function validate(value) {
-    if (type === false) return true;
+    if (type === false) return true; // caso a valição receba false como parametro, ele n faz a validação
     if (value.length === 0) {
       setError('Preencha um valor');
       return false;
@@ -24,7 +25,7 @@ const useForm = (type) => {
       return true;
     }
   }
-
+  // faz a validação conforme ocorram eventos de onCHnage nos inputs, seta os dados no setValue
   function onChange({ target }) {
     if (error) validate(target.value);
     setValue(target.value);
