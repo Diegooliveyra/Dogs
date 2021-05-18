3da668f96a1e5b0bd4a5da32f1f35433bd93217f
diegooliveyra <diegooliveirabryto@gmail.com>
2021-05-18 15:51:17 -0300

    modal finalizado

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -6,15 +6,22 @@ import Loading from '../Helper/Loading';
 import PhotoContent from '../Photo/PhotoContent';
 import styles from './FeedModal.module.css';
 
-const FeedModal = ({ photo }) => {
+const FeedModal = ({ photo, setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
     const { url, options } = PHOTO_GET(photo.id);
     request(url, options);
   }, [photo, request]);
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) {
+      setModalPhoto(null);
+    }
+  }
+
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -7,12 +7,12 @@ const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
   return (
     <div className={styles.photo}>
-      <div className={styles.photo}>
+      <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
