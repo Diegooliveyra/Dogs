a5200b80643bb715a40688c9da27a5227b5fbe60
diegooliveyra <diegooliveirabryto@gmail.com>
2021-06-18 12:59:19 -0300

    feito o footer

--- src/App.js
+++ src/App.js
@@ -13,18 +13,20 @@ import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <ProtectedRoute path="conta/*" element={<User />} />
-            <Route path="foto/:id" element={<Photo />} />
-            <Route path="perfil/:user" element={<UserProfile />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <ProtectedRoute path="conta/*" element={<User />} />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import style from './Footer.module.css';
+import { ReactComponent as Dogs } from '../../Assets/dogs-footer.svg';
 
 const Footer = () => {
-  return <div className={style.footer}></div>;
+  return (
+    <footer className={style.footer}>
+      <Dogs />
+      <p>Dog. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
