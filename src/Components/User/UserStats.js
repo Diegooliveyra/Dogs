86500b4e64ffdb75d0931f58f08bd98ec3c6cbd9
diegooliveyra <diegooliveirabryto@gmail.com>
2021-06-22 15:29:12 -0300

    adc React lazy no componente UserStats

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -5,7 +5,7 @@ import Error from '../Helper/Error';
 import useFetch from '../../Hooks/useFetch';
 import { useEffect } from 'react';
 import { STATS_GET } from '../../Api/api';
-import UserStatsGraphs from './UserStatsGraphs';
+const UserStatsGraphs = React.lazy(()=> import('./UserStatsGraphs'))
 
 const UserStats = () => {
   const { data, loading, error, request } = useFetch();
@@ -22,10 +22,10 @@ const UserStats = () => {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="Estatistica" />
         <UserStatsGraphs data={data} />
-      </div>
+      </React.Suspense>
     );
   else return null;
 };
